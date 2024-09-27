c12be68f39a06ee603b93db23058f2d9382a8b72
PizzaWunsch <lucashensel0815@gmail.com>
2024-09-27 20:39:46 +0200

    fixed ColdWeaponsScenario with anvil craft

--- src/main/java/dev/pizzawunsch/scenarios/ColdWeaponsScenario.java
+++ src/main/java/dev/pizzawunsch/scenarios/ColdWeaponsScenario.java
@@ -3,6 +3,7 @@ package dev.pizzawunsch.scenarios;
 import com.google.common.collect.Lists;
 import dev.pizzawunsch.UHCenario;
 import dev.pizzawunsch.utils.scenario.Scenario;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -10,6 +11,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
@@ -31,6 +33,24 @@ public class ColdWeaponsScenario extends Scenario implements Listener {
             for (org.bukkit.enchantments.Enchantment enchantment : event.getItem().getEnchantments().keySet()) {
                 if(enchantment.equals(Enchantment.FIRE_ASPECT) || enchantment.equals(Enchantment.ARROW_FIRE))
                     event.getItem().removeEnchantment(enchantment);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if(!this.isEnabled())
+            return;
+        if(event.getWhoClicked() instanceof Player) {
+
+            if(event.getCurrentItem() == null)
+                return;
+
+            if(event.getCurrentItem().getEnchantments().containsKey(Enchantment.FIRE_ASPECT)) {
+                event.getCurrentItem().removeEnchantment(Enchantment.FIRE_ASPECT);
+            }
+            if(event.getCurrentItem().getEnchantments().containsKey(Enchantment.ARROW_FIRE)) {
+                event.getCurrentItem().removeEnchantment(Enchantment.ARROW_FIRE);
             }
         }
     }
