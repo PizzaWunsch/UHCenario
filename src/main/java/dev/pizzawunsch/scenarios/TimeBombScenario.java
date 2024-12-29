8ac9708df6588f79747297b9ec1a0308c0ad4b74
Lucas <lucashensel0815@gmail.com>
2024-12-29 13:15:26 +0100

    tries to fix timebomb

--- src/main/java/dev/pizzawunsch/scenarios/TimeBombScenario.java
+++ src/main/java/dev/pizzawunsch/scenarios/TimeBombScenario.java
@@ -23,28 +23,32 @@ import java.util.List;
  * This class handles the time bomb scenario.
  *
  * @author Lucas | PizzaWunsch
- * @version 1.0
+ * @version 1.2
  * @since 21.10.2022
  */
 public class TimeBombScenario extends Scenario implements Listener {
 
+
+    private boolean trapped;
     /**
      * Adding the scenario into the local storage list.
      */
     public TimeBombScenario() {
         super("timebomb");
+        trapped= false;
     }
 
+
     /**
-     * Spawns a chest with the contents of player and explode after 30 seconds.
+     * Spawns a double chest with the contents of the player and explodes after 30 seconds.
      *
-     * @param event the corresponding tome bomb event
+     * @param event the corresponding time bomb event
      */
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         // the player who died
         Player player = event.getEntity();
-        // is scenario is enabled
+        // is scenario enabled
         if (this.isEnabled()) {
             // A list with all drops of a player
             List<ItemStack> drops = Lists.newArrayList();
@@ -56,21 +60,27 @@ public class TimeBombScenario extends Scenario implements Listener {
             Location location = player.getLocation().add(0, 1, 0);
             // get the block from the location
             Block block = location.getBlock();
-            // get the down relative of block
-            block.getRelative(BlockFace.DOWN);
 
-            List<Block> chestClock = Lists.newArrayList();
+            trapped = !trapped;
+
+            // Find a valid location for the double chest
+            block = findValidChestLocation(block);
+
+            List<Block> chestBlocks = Lists.newArrayList();
 
             // sets the type of the block to a chest
-            block.setType(Material.CHEST);
-            chestClock.add(block);
-            // the chest of by blocks state
+            block.setType((trapped ? Material.CHEST : Material.TRAPPED_CHEST));
+            chestBlocks.add(block);
+            // the chest of the block's state
             Chest chest = (Chest) block.getState();
-            // get the relative of northern side from block
-            block = block.getRelative(BlockFace.NORTH);
-            // set the type of the relative block to chest
-            block.setType(Material.CHEST);
-            chestClock.add(block);
+
+            // Place the second chest adjacent to the first one
+            Block adjacentBlock = findValidAdjacentBlock(block);
+            if (adjacentBlock != null) {
+                adjacentBlock.setType((trapped ? Material.CHEST : Material.TRAPPED_CHEST));
+                chestBlocks.add(adjacentBlock);
+            }
+
             // loops all item stacks from the drop list
             for (ItemStack itemStack : drops) {
                 if (itemStack == null || itemStack.getType() == Material.AIR)
@@ -79,7 +89,7 @@ public class TimeBombScenario extends Scenario implements Listener {
                 // adds the item from drop list to chest
                 chest.getInventory().addItem(itemStack);
             }
-            // spawns a new armor stand to display the time how long the time bomb exist
+            // spawns a new armor stand to display the time how long the time bomb exists
             ArmorStand armorStand = player.getWorld().spawn(chest.getLocation().clone().add(0.5, 1, 0),
                     ArmorStand.class);
             // set some relevant variables to the armor stand
@@ -88,7 +98,7 @@ public class TimeBombScenario extends Scenario implements Listener {
             armorStand.setGravity(false);
             armorStand.setVisible(false);
             armorStand.setMarker(true);
-            // creates a new task to change the name of armor stand
+            // creates a new task to change the name of the armor stand
             new BukkitRunnable() {
                 // time variable
                 private int time = UHCenario.getInstance().getMainConfiguration().getConfig().getInt("scenarios.timebomb.time");
@@ -105,7 +115,7 @@ public class TimeBombScenario extends Scenario implements Listener {
                                 location.getZ() + 0.5, 4, true, true);
                         // spawns a new lightning
                         location.getWorld().strikeLightning(location);
-                        for(Block block1 : chestClock) {
+                        for (Block block1 : chestBlocks) {
                             block1.setType(Material.AIR);
                         }
                         // removes the armor stand
@@ -113,9 +123,51 @@ public class TimeBombScenario extends Scenario implements Listener {
                         // cancels the task
                         cancel();
                     } else
-                        armorStand.setCustomName(UHCenario.getInstance().getMessage("scenarios.timebomb.timer").replaceAll("%player%", player.getName()).replaceAll("%time%", time + ""));
+                        armorStand.setCustomName(UHCenario.getInstance().getMessage("scenarios.timebomb.timer")
+                                .replaceAll("%player%", player.getName())
+                                .replaceAll("%time%", time + ""));
                 }
             }.runTaskTimer(UHCenario.getInstance(), 0, 20);
         }
+    }
+
+    /**
+     * Finds a valid location for placing the first chest.
+     *
+     * @param block the initial block location
+     * @return a valid block for placing the chest
+     */
+    private Block findValidChestLocation(Block block) {
+        // Check if the block is already occupied
+        if (block.getType() == Material.AIR) {
+            return block;
+        }
+
+        // Try adjacent blocks in order
+        for (BlockFace face : new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.UP}) {
+            Block relative = block.getRelative(face);
+            if (relative.getType() == Material.AIR) {
+                return relative;
+            }
+        }
+
+        // Default to the original block if no valid location is found
+        return block;
+    }
+
+    /**
+     * Finds a valid adjacent block for the second chest.
+     *
+     * @param block the block where the first chest is placed
+     * @return a valid block for placing the second chest or null if none is found
+     */
+    private Block findValidAdjacentBlock(Block block) {
+        for (BlockFace face : new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST}) {
+            Block relative = block.getRelative(face);
+            if (relative.getType() == Material.AIR) {
+                return relative;
+            }
+        }
+        return null;
     }
 }
